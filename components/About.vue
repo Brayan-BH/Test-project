623f1dc820d114abeb03452b506b5a9c68f5150b
Brayan-BH <brayangithub007@gmail.com>
2023-02-04 18:37:15 -0500

    upload files

---
+++ components/About.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="section-about">
+        <div class="container-about">
+            <div class="header-about">
+                <h3>
+                    Somos New Capital Tours
+                </h3>
+                <p>
+                    Somos una empresa de taxi formal, 100% peruana y acreditada ante la ATU. Tenemos relaciones
+                    comerciales con empresas importantes en Perú, contamos con "Cero incidencias" registradas durante
+                    estos más de 25 años de operaciones.
+                </p>
+            </div>
+            <div class="row-about">
+                <div class="content-list">
+                    <div class="title-about">
+                        <h3>Autos Desinfectados, en cada viaje</h3>
+                        <p>
+                            Cumplimos con todos los protocolos de salubridad, para poder cuidar a nuestros usuarios en
+                            cada
+                            viaje.
+                        </p>
+                    </div>
+                    <div class="box-info">
+                        <div class="icon">
+                            <i class="material-icons">
+                                handshake
+                            </i>
+                        </div>
+                        <h4 class="title">
+                            Seguridad y Calidad
+                        </h4>
+                        <p class="description">
+                            Constituye uno de los pilares dentro de la empresa en que nos apoyamos para
+                            la competitividad de nuestros servicios.
+                        </p>
+                    </div>
+                    <div class="box-info">
+                        <div class="icon">
+                            <i class="material-icons">
+                                lightbulb
+                            </i>
+                        </div>
+                        <h4 class="title">
+                            Salubridad y Experiencia
+                        </h4>
+                        <p class="description">
+                            Constituye uno de los pilares dentro de la empresa en que nos apoyamos para
+                            la competitividad de nuestros servicios.
+                        </p>
+                    </div>
+                    <div class="box-info">
+                        <div class="icon">
+                            <i class="material-icons">
+                                check_circle
+                            </i>
+                        </div>
+                        <h4 class="title">
+                            Aprobados en el SISCOVID
+                        </h4>
+                        <p class="description">
+                            Constituye uno de los pilares dentro de la empresa en que nos apoyamos para
+                            la competitividad de nuestros servicios.
+                        </p>
+                    </div>
+                </div>
+                <div class="img-fluid">
+                    <img src="img/caracteristicas/about-img.jpg" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/Intro.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- Plactont info -->
+    <section>
+        <div class="main-head">
+            <!-- <div class="head-banner">
+                <p class="banner__text">
+                    Pariatur in dolore incididunt laboris deserunt aliquip voluptate esse anim tempor do fugiat
+                    reprehenderit sapeeee.
+                </p>
+                <p>Sapeeee 2</p>
+                <p class="banner__link">
+                    <span class="link__text">Obtener más información</span>
+                    <span class="link__icon material-icons">navigate_next</span>
+                </p>
+            </div> -->
+            <div class="head-img">
+                <div class="img__gradient"></div>
+            </div>
+            <div class="head-detail">
+                <div class="main-container">
+                    <div class="container-content">
+                        <div class="text">
+                            <h1>Capital Tours</h1>
+                            <p>Conduce Con Nosotros ó <br> solicita tu servicio</p>
+                            <div class="text__buttons">
+                                <a>
+                                    <button class="button-1" type="button" role="button">
+                                        Afiliate
+                                    </button>
+                                </a>
+                                <a>
+                                    <button class="button-1" type="button" role="button">
+                                        Contactanos
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="text__img">
+                            <!-- <img src="/img/svg/imagen2.svg" alt="svg"> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
